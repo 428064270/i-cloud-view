9ee51316d1ac5e746a96004df649c3543ed867fd
zhaotianyu <428064270@qq.com>
2020-12-11 17:23:00 +0800

    删除无用配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
-import da from 'element-ui/src/locale/lang/da'
 
 // create an axios instance
 const service = axios.create({
